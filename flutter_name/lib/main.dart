6c023bf634bd6930b75939bc36aa2822194d3521
JacobOdd <antonpichka@gmail.com>
2023-02-11 20:49:52 +0200

    refactoring(flutter_name): delete package

--- flutter_name/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_name/named_view/app_view.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(AppView());
-}
