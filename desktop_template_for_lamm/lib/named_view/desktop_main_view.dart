b7b84807df4dddf9dcb3d33e5bce9215eb40834a
JacobOdd <antonpichka@gmail.com>
2023-03-04 16:17:04 +0200

    refactoring(desktop_template_for_lamm): fix import and adding named_view_list_view_model and update analysis_options.yaml

--- desktop_template_for_lamm/lib/named_view/desktop_main_view.dart
+++ desktop_template_for_lamm/lib/named_view/desktop_main_view.dart
@@ -1,4 +1,4 @@
-import 'package:common_template_for_lamm/named_view_list_view_model/desktop_main_view_list_view_model.dart';
+import 'package:desktop_template_for_lamm/named_view_list_view_model/desktop_main_view_list_view_model.dart';
 import 'package:flutter/material.dart';
 
 class DesktopMainView
