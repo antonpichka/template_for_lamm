6b7fd1b0ad30a9cbfc8f6450de4729d4ca3d8627
antonpichka <antonpichka@gmail.com>
2023-10-13 18:43:33 +0300

    feature(.): update project where LAMM v0.1.9

---
+++ common_template_for_lamm/lib/named_utility/keys_name_stream_to_temp_cache_service_utility.dart
@@ -0,0 +1,9 @@
+import 'package:meta/meta.dart';
+
+@immutable
+final class KeysNameStreamToTempCacheServiceUtility {
+  /* AppViewListViewModel */
+  static const String appViewListViewModelQUniqueIdByUser = "appViewListViewModelQUniqueIdByUser";
+
+  const KeysNameStreamToTempCacheServiceUtility._();
+}
