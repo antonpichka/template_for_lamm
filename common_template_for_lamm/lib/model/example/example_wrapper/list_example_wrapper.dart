cf8a7ba0c90e967a03bafd1c738dae7e7c21d703
antonpichka <antonpichka@gmail.com>
2025-01-11 04:01:34 +0200

    refactoring(.): release v6.1.2

--- common_template_for_lamm/lib/model/example/example_wrapper/list_example_wrapper.dart
+++ common_template_for_lamm/lib/model/example/example_wrapper/list_example_wrapper.dart
@@ -1,4 +1,5 @@
 import 'package:common_template_for_lamm/model/example/example.dart';
+import 'package:common_template_for_lamm/model/example/example_wrapper/example_wrapper.dart';
 import 'package:common_template_for_lamm/model/example/list_example.dart';
 import 'package:library_architecture_mvvm_modify/library_architecture_mvvm_modify.dart';
 import 'package:meta/meta.dart';
@@ -11,7 +12,8 @@ base class ListExampleWrapper extends BaseListModelWrapper {
   ListExample createListModel() {
     final List<Example> listModel = List.empty(growable: true);
     for (final List<dynamic> itemListObject in listsListObject) {
-      listModel.add(Example(itemListObject[0]));
+      final exampleWrapper = ExampleWrapper(itemListObject);
+      listModel.add(exampleWrapper.createModel());
     }
     return ListExample(listModel);
   }
